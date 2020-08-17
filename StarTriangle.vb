ace9c0e5c362e9539cc57184ee8fa4daa2c7ecba
Prerak Gada <58708396+PrerakGada@users.noreply.github.com>
2020-08-17 17:45:06 +0530

    Star Triangle Pattern - Console Application

---
+++ StarTriangle.vb
@@ -0,0 +1,26 @@
+﻿Imports System.Console
+Module StarTriangle
+    Sub main()
+        WriteLine("Hello Prerak!")
+
+        Dim i As Integer, j As Integer
+
+        Write("Enter the size: ")
+        Dim n As Integer = ReadLine()
+
+        For i = 1 To n Step 1
+
+            For j = n To i Step -1
+                Write(" ")
+            Next
+
+            For j = 1 To i Step 1
+                Write("* ")
+            Next
+
+            WriteLine()
+        Next
+
+        ReadLine()
+    End Sub
+End Module
